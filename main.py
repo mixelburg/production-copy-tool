3db64773c70252f6cebcaaf185001407708caeff
mixelburg <mixelburg.play@gmail.com>
2022-08-07 17:03:09 +0300

    path fix

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ with open(os.path.join(DATA_FOLDER_PATH, 'db.json')) as f:
         user_db_folder = os.path.join(DATA_FOLDER_PATH, 'db', 'accounts', user_id)
         user_db_file = os.path.join(user_db_folder, 'db.json')
 
-        path = os.path.join(REMOTE_PATH, 'db', 'accounts', user_id, 'db.json')
-        command = f'rsync -aP -e "{SSH_COMMAND}" --rsync-path="mkdir -p {user_db_folder} && rsync" {user_db_file} {path}'
+        path = os.path.join(REMOTE_PATH, 'db', 'accounts', user_id)
+        command = f'rsync -aP -e "{SSH_COMMAND}" --rsync-path="mkdir -p {path} && rsync" {user_db_file} {path}'
         print(command)
         os.system(command)
 
